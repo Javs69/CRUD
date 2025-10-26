633a5adf45cd29b9afe5a353b3253a2d5c1d438a
Javs69 <oswamtz222@gmail.com>
2025-10-25 23:00:09 -0600

    Intento no.2  de botón para agregar a la base de datos :3

---
+++ crear.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/../index.php'; // Esto tambíen fue de un video y nos sirve para conectar a la base de datos
+
+$nombre   = $_POST['nombre']   ?? '';
+$apellido = $_POST['apellido'] ?? '';
+$genero   = $_POST['genero']   ?? '';
+$telefono = $_POST['telefono'] ?? '';
+
+$sql = "INSERT INTO usuarios (nombre, apellido, genero, telefono) VALUES ($1,$2,$3,$4)";
+$r = pg_query_params($conexion, $sql, [$nombre, $apellido, $genero, $telefono]);
+
+if ($r) { http_response_code(201); echo 'OK'; }
+else    { http_response_code(400); echo 'ERROR'; }
