1e3e5dc78e3f343c5b556c1d75a5adb2f6c01222
Javs69 <oswamtz222@gmail.com>
2025-11-29 10:44:36 -0600

    Render db agregada

--- conexion.php
+++ conexion.php
@@ -1,4 +1,6 @@
 <?php
-$conexion = pg_connect("host=localhost port=5432 dbname=crud user=postgres password=200612");
+$conexion = pg_connect(
+  "host=dpg-d4lhvj8gjchc73aorq80-a.oregon-postgres.render.com port=5432 dbname=crud_dmkk user=crud_dmkk_user password=mA3xbQFGTk7HEPDL0FS109ODZ8sYH2vR sslmode=require"
+);
 
 ?>
--- editar.php
+++ editar.php
@@ -21,11 +21,4 @@ $sql = "UPDATE usuarios
         SET nombre=$1, apellido=$2, genero=$3, telefono=$4
         WHERE id=$5";
 $res = pg_query_params($conexion, $sql, [$nombre, $apellido, $genero, $telefono, (int)$id]);
-
-if ($res === false) {
-  http_response_code(400);
-  exit('DB_UPDATE_ERROR: ' . pg_last_error($conexion));
-}
-
-http_response_code(200);
-echo 'OK';
+?>
